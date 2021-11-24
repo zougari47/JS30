84a2181a1c1748cba0e8ca14beba22a2601be7d4
zougari47 <zougariahmed007@gmail.com>
2021-11-24 18:11:02 +0000

    finish the Type_Ahead project

--- 06 - Type Ahead/app.js
+++ 06 - Type Ahead/app.js
@@ -32,7 +32,9 @@ function findMatches(wordToMatch, cities){
     });
 }
 
-
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
 function displayMatches(){
     const matchArray = findMatches(this.value, cities);
@@ -47,7 +49,7 @@ function displayMatches(){
         return `
         <li>
             <span class="name">${cityName}, ${stateName}</span>
-            <span class="population">${place.population}</span>
+            <span class="population">${numberWithCommas(place.population)}</span>
         </li>
         ` ;
     }).join('');
