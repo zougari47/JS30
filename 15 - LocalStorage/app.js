32475e9cde3a28d606ad79a302349799e155a25f
zougari47 <zougariahmed007@gmail.com>
2022-01-09 19:04:54 +0000

    edit: project 15
    add 3 functionality check all/uncheck all/ clear all

--- 15 - LocalStorage/app.js
+++ 15 - LocalStorage/app.js
@@ -1,16 +1,18 @@
-const addItems = document.querySelector(".add-items");
-const itemsList = document.querySelector(".plates");
-const items = [];
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+const btns = document.querySelectorAll('button');
 
 function addItem(e) {
   e.preventDefault(); //stop the reload
-  const text = this.querySelector("[name=item]").value;
+  const text = this.querySelector('[name=item]').value;
   const item = {
     text, //ES6
     done: false,
   };
   items.push(item);
   populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
   this.reset();
 }
 
@@ -19,18 +21,39 @@ function populateList(plates = [], platesList) {
     .map((plate, i) => {
       return `
         <li>
-        <input type="chekbox" data-index=${i} id="item${i}"
-        ${plate.done ? "checked" : ""} />
+        <input type="checkbox" data-index=${i} id="item${i}" ${
+        plate.done ? 'checked' : ''
+      } />
         <label for="items${i}">${plate.text}</label>
         </li>
         `;
     })
-    .join("");
+    .join('');
 }
 
-let checkbox = document.querySelectorAll("input[type='checkbox']");
-// items.forEach((item, i) => {
+function toggleDone(e) {
+  if (!e.target.matches('input')) return; //skip this unless it's an input
+  const element = e.target;
+  const index = element.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
 
-// });
+function doAll(e) {
+  // check all
+  if (e.target.innerText === 'Check all') {
+    //check all code here
+    console.log(true);
+  } else if (e.target.innerText === 'Uncheck all') {
+    //Uncheck all code here
+  } else if (e.target.innerText === 'Clear all') {
+    //Clear all code here
+  }
+  // console.log(e.target);
+}
 
-addItems.addEventListener("submit", addItem);
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+btns.forEach((btn) => btn.addEventListener('click', doAll));
+populateList(items, itemsList);
