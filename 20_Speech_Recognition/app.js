4dadf8bad7e5597f04af60a705cc2dd5afc19c64
zougari47 <zougariahmed007@gmail.com>
2022-01-16 21:35:20 +0000

    add project 20

---
+++ 20_Speech_Recognition/app.js
@@ -0,0 +1,30 @@
+window.alert(
+  'This Speech Detection work only in Google Chrome\nMake sure you open the site in Google Chrome browsers\nThank you 🙏'
+);
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+  p.textContent = transcript;
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
