0a1e0795cf20a74ea318a6b3fe35360b384e1abd
zougari47 <zougariahmed007@gmail.com>
2022-01-17 18:15:20 +0000

    add project 21

---
+++ 21_Geolocation/app.js
@@ -0,0 +1,14 @@
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+console.log(speed, arrow);
+
+navigator.geolocation.watchPosition(
+  data => {
+    console.log(data.coords.speed);
+    speed.textContent = data.coords.speed.toFixed(3);
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  err => {
+    alert('Hey! you gotta allow that to happen!');
+  }
+);
