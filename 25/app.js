a1e26890713b5ba8715b0da74127649279164a16
zougari47 <zougariahmed007@gmail.com>
2022-01-21 18:19:53 +0000

    add project 25

---
+++ 25/app.js
@@ -0,0 +1,13 @@
+const divs = document.querySelectorAll('div');
+
+function LogText(e) {
+  console.log(this.classList.value);
+  //   e.stopPropagation();   //stop Bubbling!
+}
+
+divs.forEach(div => div.addEventListener('click', LogText));
+document.body.addEventListener('click', LogText);
+
+// div => div.addEventListener('click', LogText, {
+//     once: true //run the function one time after the event
+// })
